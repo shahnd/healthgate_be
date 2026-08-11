8c549078618a8ba18bed63cfbf50dc2b65b3f7c1
Sword672 <ansunghn@gmail.com>
2026-08-12 00:15:46 +0900

    feat: SecurityConfig 설정 추가

--- src/main/java/com/kh/healthgate/config/SecurityConfig.java
+++ src/main/java/com/kh/healthgate/config/SecurityConfig.java
@@ -1,5 +1,25 @@
 package com.kh.healthgate.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+public class SecurityConfig {
+	// BCryptPasswordEncoder 를 빈으로 등록해주는 메소드
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+		
+		return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+				   .csrf(csrf -> csrf.disable())
+				   .build();
+	}
 }
