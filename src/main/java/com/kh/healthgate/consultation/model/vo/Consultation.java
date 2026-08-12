b9ef5f70727b895491e82db294e03118c4e2dd8f
김유화 <ua0731@gmail.com>
2026-08-12 12:10:34 +0900

    보건상담 Entity 컬럼명 수정
    - 명명규칙 변경에 따른 수정

--- src/main/java/com/kh/healthgate/consultation/model/vo/Consultation.java
+++ src/main/java/com/kh/healthgate/consultation/model/vo/Consultation.java
@@ -37,9 +37,9 @@ import lombok.ToString;
 public class Consultation {
 
 	@Id
-	@Column(name="CONSULTATIONS_ID")
+	@Column(name="CONSULTATION_ID")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long consultationsId;		// consultations_id	상담예약번호(ID)
+	private Long consultationId;		// consultations_id	상담예약번호(ID)
 	
 	@ManyToOne
 	@JoinColumn(name="EMPLOYEE_ID", nullable=false)
@@ -49,30 +49,30 @@ public class Consultation {
 	@JoinColumn(name="MANAGER_ID", nullable=false)
 	private Employee manager;			// manager	상담사(FK, NN)
 	
-	@Column(name="SCHEDULED_DATE", nullable=false,
+	@Column(name="CONSULTATION_SCHEDULED_DATE", nullable=false,
 			columnDefinition="DATE")
-	private Date scheduledDate;			// scheduled_date	예약일자(NN, DATE)
+	private Date consultationScheduledDate;			// scheduled_date	예약일자(NN, DATE)
 	
-	@Column(name="SCHEDULED_TURN", nullable=false,
+	@Column(name="CONSULTATION_SCHEDULED_TURN", nullable=false,
 			columnDefinition="CHAR(2)")
-	private String scheduledTurn;		// scheduled_turn	예약순번(NN, CHAR(2) 'T1'/ 'T2/ ... / 'T6'(1차 ~ 6차)
+	private String consultationScheduledTurn;		// scheduled_turn	예약순번(NN, CHAR(2) 'T1'/ 'T2/ ... / 'T6'(1차 ~ 6차)
 	
-	@Column(name="REASON", nullable=false, length=100)
-	private String reason;				// reason	신청사유(NN, VARCHAR(100))
+	@Column(name="CONSULTATION_REASON", nullable=false, length=100)
+	private String consultationReason;				// reason	신청사유(NN, VARCHAR(100))
 	
-	@Column(name="CONTENT",
+	@Column(name="CONSULTATION_CONTENT",
 			columnDefinition="TEXT")
-	private String content;				// content	상담내용(NN, TEXT)
+	private String consultationContent;				// content	상담내용(NN, TEXT)
 	
 	@Enumerated(EnumType.STRING)
-	@Column(name="STATUS", nullable=false, length=20,
+	@Column(name="CONSULTATION_STATUS", nullable=false, length=20,
 			columnDefinition="VARCHAR(20) DEFAULT 'RESERVED'")
-	private ConsultationStatus status;				// status	상담진행상태(NN), DEFAULT RESERVED
+	private ConsultationStatus consultationStatus;				// status	상담진행상태(NN), DEFAULT RESERVED
 	
-	@Column(name="CONSULTATION_AT", columnDefinition="TIMESTAMP")
-	private LocalDateTime consultationAt;		// requested_at	일지작성일시(CURRENT_TIMESTAMP)
+	@Column(name="CONSULTATION_CONSULTATION_AT", columnDefinition="DATETIME")
+	private LocalDateTime consultationConsultationAt;		// requested_at	일지작성일시(CURRENT_TIMESTAMP)
 	
 	@Column(name="CREATED_AT", nullable=false,
-			columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-	private LocalDateTime createdAt;		// logged_at	상담예약신청일시(CURRENT_TIMESTAMP)
+			columnDefinition="DATETIME DEFAULT CURRENT_TIMESTAMP")
+	private LocalDateTime consultationCreatedAt;		// logged_at	상담예약신청일시(CURRENT_TIMESTAMP)
 }
