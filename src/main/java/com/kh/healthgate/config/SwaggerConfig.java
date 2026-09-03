bdab846dbfd1ac34e98f8c1ed5bae7a841e27bcd
안성훈 <ansunghn@gmail.com>
2026-09-03 11:45:46 +0900

    feat: swagger 초기 설정

--- src/main/java/com/kh/healthgate/config/SwaggerConfig.java
+++ src/main/java/com/kh/healthgate/config/SwaggerConfig.java
@@ -1,5 +1,35 @@
 package com.kh.healthgate.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
 
+    private static final String JWT_SCHEME_NAME = "JWT";
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityRequirement securityRequirement =
+                new SecurityRequirement().addList(JWT_SCHEME_NAME);
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name(JWT_SCHEME_NAME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("HealthGate API")
+                        .description("직원 건강관리 시스템 API 문서")
+                        .version("v1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(new Components().addSecuritySchemes(JWT_SCHEME_NAME, securityScheme));
+    }
 }
