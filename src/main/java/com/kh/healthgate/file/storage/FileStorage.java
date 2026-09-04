2944ca8071a60dddd7d0ed20bf06b785f8a27f3b
송경민 <songkm2@naver.com>
2026-09-04 16:50:54 +0900

    refactor: FileStorage에 MultipartFile을 받는 default 메서드 추가

--- src/main/java/com/kh/healthgate/file/storage/FileStorage.java
+++ src/main/java/com/kh/healthgate/file/storage/FileStorage.java
@@ -1,11 +1,26 @@
 package com.kh.healthgate.file.storage;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kh.healthgate.file.exception.FileStorageException;
 
 public interface FileStorage {
     StoredFile store(String filename, String contentType, InputStream inputStream);
+
+    default StoredFile store(MultipartFile file) {
+        try {
+            return store(
+                    file.getOriginalFilename(),
+                    file.getContentType(),
+                    file.getInputStream());
+        } catch (IOException exception) {
+            throw new FileStorageException("업로드 파일을 읽지 못했습니다.", exception);
+        }
+    }
 
     Resource load(String storageKey);
 
