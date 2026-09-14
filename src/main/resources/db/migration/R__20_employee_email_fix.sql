279e23bdbf55f76658a06ce4e0186fd2a91d9832
송경민 <songkm2@naver.com>
2026-09-14 09:25:21 +0900

    chore: 시연용 검진 미완료 직원 3명 이메일 설정
    - DB 초기화를 막기 위해서 새 파일로 추가합니다.

---
+++ src/main/resources/db/migration/R__20_employee_email_fix.sql
@@ -0,0 +1,3 @@
+UPDATE IGNORE employees SET email = 'dbwhdrb1@gmail.com' WHERE employee_number = '0002';
+UPDATE IGNORE employees SET email = 'dbwhdrb1@gmail.com' WHERE employee_number = '0013';
+UPDATE IGNORE employees SET email = 'dbwhdrb1@gmail.com' WHERE employee_number = '0031';
