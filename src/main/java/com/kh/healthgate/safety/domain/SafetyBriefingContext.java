94e755d489586b584b3d64ad0ff122ef87ee7736
송경민 <songkm2@naver.com>
2026-09-14 09:52:44 +0900

    fix: 안전브리핑 출력 형식을 제목과 수칙 목록으로 변경

--- src/main/java/com/kh/healthgate/safety/domain/SafetyBriefingContext.java
+++ src/main/java/com/kh/healthgate/safety/domain/SafetyBriefingContext.java
@@ -17,7 +17,7 @@ public record SafetyBriefingContext(
         List<String> documentFingerprints,
         String documentMetadata) {
 
-    private static final String CONTEXT_VERSION = "v12";
+    private static final String CONTEXT_VERSION = "v13";
 
     public SafetyBriefingContext {
         documentFingerprints = documentFingerprints.stream()
